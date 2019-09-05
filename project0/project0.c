6e1f049340e85bdf0cecd5aac15b604f9292cea1
Miguel Barrios Davila <miguelbarrios67@gmail.com>
2019-09-04 22:04:25 -0500

    Input loop completed

--- project0/project0.c
+++ project0/project0.c
@@ -6,7 +6,15 @@ int main(int argc, char *argv[])
 
         if(argc == 1)
         {
-                printf("Output will be in lowercase\n");
+                printf("Started input numbers\n");
+
+                 char name[10];
+                 printf("Who are you? ");
+                 //end of file is simulated by pressing CTRL-d
+                while(fgets(name,10,stdin))
+                {
+                    printf("Glad to meet you, %s",name);
+                }
         }
         else if(*argv[1] != 117 || argc > 2)
         {
