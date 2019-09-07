97054aa1d533fe42ad407d4e8271973eaa96c680
Miguel Barrios Davila <miguelbarrios67@gmail.com>
2019-09-07 18:36:47 -0500

    working 99%

--- project0/project0.c
+++ project0/project0.c
@@ -4,54 +4,52 @@
 int main(int argc, char *argv[])
 {
     char * ones[] = {"","one ", "two ", "three ", "four ", "five ", "six " , "seven ","eight ", "nine " };
-    char * teens[] = {"ten ", "elleven ", "twelve ", "thirteen ", "fourteen ", "fifteen ", "sixteen ", "seventeen ", "eighteen ", "nineteen "};
+    char * teens[] = {"ten ", "elleven ", "twelve ", "thirteen ", "fourteen ", "fifteen ", "sixteen ",
+        "seventeen ", "eighteen ", "nineteen "};
     char * tens[] = {"ten ", "twenty ", "thirty ", "forty ", "fifty ", "sixty ", "seventy ", "eighty ", "ninety "};
-    char * hundredPlus[] = {"hundred ", "thousand ", "million "};
+    char * powerTen[] = {"hundred ", "thousand ", "million "};
     
     if(argc == 1 || (argc == 2 && *argv[1] == 117))
     {
-        
         char numberString[11];
-        printf("%s\n", "input number: ");
-        //end of file is simulated by pressing CTRL-d
+        char ouputString[200];
+        
         while(fgets(numberString,11,stdin))
         {
-            char ouputString[200];
-            ouputString[0] = '\0';
-            int firstNonZero = 0;
-            for(int i = 0; i < strlen(numberString) - 1; ++i)
+            ouputString[0] = '\0';            //resets string
+            int length = (int)strlen(numberString) - 1;
+            int firstNonZeroIndex = -1;
+            for(int i = 0; i < length; ++i)
             {
                 if(numberString[i] != 48)
                 {
-                    firstNonZero = i;
+                    firstNonZeroIndex = i;
                     break;
                 }
             }
-            
-            int inputNumber[] = {-1, -1, -1, -1, -1, -1, -1, -1, -1};
-            int power = ((int)strlen(numberString) - 1) - firstNonZero;
-            int offset = 9 - power;
-            
-            //converts string to int array with correct number
-            for(int i = firstNonZero; i < strlen(numberString) - 1; ++i, ++offset)
+            //number is not zero
+            if(firstNonZeroIndex != -1)
             {
-                int currentNum =  numberString[i] % 48;
-                inputNumber[offset] = currentNum;
-            }
-            
-            for(int index = firstNonZero; index < 9; ++index)
-            {
-                int currentNumber = inputNumber[index];
-                if(currentNumber != -1)
+                int inputNumber[] = {-1, -1, -1, -1, -1, -1, -1, -1, -1};
+                int offset = 9 - (length - firstNonZeroIndex);
+                
+                //converts string to int array with correct number
+                for(int i = firstNonZeroIndex, j = offset; i < length; ++i, ++j)
                 {
+                    inputNumber[j] = numberString[i] % 48;
+                
+                }
+                
+                for(int index = offset; index < 9; ++index)
+                {
+                    int currentNumber = inputNumber[index];
                     ////output hundred
                     if((index == 0 || index == 3 || index == 6) && (currentNumber != 0))
                     {
                         strcat(ouputString, ones[currentNumber]);
-                        strcat(ouputString, hundredPlus[0]);
-                        
+                        strcat(ouputString, powerTen[0]);
                     }
-                
+                    
                     //output tens
                     int isTeens = -1;
                     if((index == 1 || index == 4 || index == 7) && (currentNumber != 0))
@@ -66,48 +64,49 @@ int main(int argc, char *argv[])
                             strcat(ouputString, tens[currentNumber - 1]);
                             ++index;
                             currentNumber = inputNumber[index];
-                            //output one's
+                            //apends one's digit
                             if((index == 2 || index == 5 || index == 8) && (currentNumber != 0))
                             {
                                 strcat(ouputString, ones[currentNumber]);
                             }
                         }
                     }
-                    if(index == 8 || index == 5 || index == 2)
+                    if((index == 8 || index == 5 || index == 2 ) && (currentNumber != 0))
                     {
                         int sum = currentNumber + (((inputNumber[index - 1] != -1) ? inputNumber[index - 1] : 0) * 10);
                         if(sum < 10 && sum != 0)
                             strcat(ouputString, ones[sum]);
                     }
                     
-                    
-        
-                    //output milion
+                    //appends milion if applies
                     if(index == 2)
                     {
-                        strcat(ouputString, hundredPlus[2]);
-                        
+                        strcat(ouputString, powerTen[2]);
                     }
-                    //output thousand
+                    
+                    //appends thousand if applies
                     if((index == 5) && (currentNumber != -1 || inputNumber[index -1] != -1 || inputNumber[index -2] != -1))
                     {
                         if(currentNumber != 0 || inputNumber[index -1] != 0 || inputNumber[index -2] != 0)
-                            strcat(ouputString, hundredPlus[1]);
+                            strcat(ouputString, powerTen[1]);
                     }
                     
+                    
                 }
-            }
-            //Transforms output string to upper case if u argument was imputed
-            if(argc == 2 && *argv[1] == 117)
-            {
-                for(int i = 0; i < strlen(ouputString) - 1; ++i)
+                //Transforms output string to upper case if u argument was imputed
+                if(argc == 2 && *argv[1] == 117)
                 {
-                    if(ouputString[i] != 32)
-                        ouputString[i] = ouputString[i] - 32;
+                    for(int i = 0; i < strlen(ouputString) - 1; ++i)
+                    {
+                        if(ouputString[i] != 32)
+                            ouputString[i] = ouputString[i] - 32;
+                    }
                 }
+                
+                printf("%s\n", ouputString);
             }
             
-            printf("%s\n", ouputString);
+
         }
     }
     else
@@ -117,3 +116,5 @@ int main(int argc, char *argv[])
     }
     return 0;
 }
+
+
