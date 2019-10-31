ffe51f15812b1081dc60016253181e9a164a4a81
Miguel Barrios Davila <miguelbarrios67@gmail.com>
2019-10-31 12:30:00 -0500

    simplified version of finding files implemented

--- project3/oufs_lib_support.c
+++ project3/oufs_lib_support.c
@@ -343,10 +343,20 @@ int oufs_find_file(char *cwd, char * path, INODE_REFERENCE *parent, INODE_REFERE
                             //fprintf(stderr, "Comparing %s and %s\n", directory_name, entry.name);
                             if(strcmp(entry.name, directory_name) == 0)
                             {
+                              //-------------new-------------------
+
+                                 fprintf(stderr, "Assigning grandparent %d\n", *parent);
+                                 grandparent = *parent;
+                                 fprintf(stderr, "Assigning parent %d\n", *child);
+                                 *parent = *child;
+                                 fprintf(stderr, "Assigning child %d\n", entry.inode_reference);
+                                 *child = entry.inode_reference;
+
+
+                                /*        unsimplified version                          
                                 if(*child == 0){
                                   fprintf(stderr, "Assigning child %d\n", entry.inode_reference);
                                    *child = entry.inode_reference;
-
                                 }
                                 else if(*parent == 0){
                                   fprintf(stderr, "Assigning parent %d\n", *child);
@@ -371,6 +381,7 @@ int oufs_find_file(char *cwd, char * path, INODE_REFERENCE *parent, INODE_REFERE
                                     fprintf(stderr, "Assigning child %d\n", entry.inode_reference);
                                     *child = entry.inode_reference;
                                 }
+                                */
                                 //found = 1;
                             }
 
